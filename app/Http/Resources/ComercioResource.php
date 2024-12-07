1b53af9e20c3d03b2018df2aa5bb314cecee438a
Trench <crodg1023@outlook.com>
2024-12-07 15:22:01 +0100

    Business images returned in the resource

--- app/Http/Resources/ComercioResource.php
+++ app/Http/Resources/ComercioResource.php
@@ -14,6 +14,11 @@ class ComercioResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $images = $this->images->map(function ($image) {
+            $path = public_path('uploads/' . $image->name);
+            return file_exists($path) ? asset('uploads/' . $image->name) : null;
+        })->filter()->values();
+
         return [
             'id' => $this->id,
             'user' => new UsuarioResource($this->usuario),
@@ -22,6 +27,7 @@ class ComercioResource extends JsonResource
             'phone' => $this->phone,
             'pictures' => $this->pictures,
             'price' => $this->price,
+            'images' => $images,
             'workingHours' => [
                 'opening' => $this->starting_hour,
                 'closing' => $this->closing_hour
